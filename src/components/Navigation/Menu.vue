65d0330f94fde7f20349fe729e3f2703089eadd4
JonasvonArb <mail@jonasvonarb.ch>
2022-11-15 21:32:43 +0100

    update

--- src/components/Navigation/Menu.vue
+++ src/components/Navigation/Menu.vue
@@ -14,13 +14,13 @@ const closeMenu = () => {
 
 <template>
   <div
-    v-if="route.name"
+    v-if="route.name && route.name != 'landing'"
     class="fixed flex w-screen overflow-x-scroll overflow-y-hidden top-0 left-0 z-40 h-screen text-base duration-700 border-r border-black"
     :class="
       store.activeMenu
         ? route.name != 'home'
           ? 'w-[35vw] bg-gray-300 text-black  overflow-y-scroll overflow-x-hidden'
-          : 'w-screen bg-gray-300 text-black overflow-x-scroll overflow-y-hidden'
+          : 'w-screen bg-white/20 px-10 text-black overflow-x-scroll overflow-y-hidden'
         : 'w-[20px] bg-gray-300 text-black  justify-center'
     "
   >
@@ -32,39 +32,45 @@ const closeMenu = () => {
     <transition name="fade">
       <ul
         v-show="store.activeMenu"
-        class="flex relative pt-20 pb-52 w-[35vw] flex-row overflow-visible duration-700"
+        class="flex relative mt-20 mb-52 flex-row overflow-visible duration-700 shrink-1"
         :class="
           route.name != 'home' ? '-translate-x-[70vw]' : '-translate-x-[0]'
         "
       >
         <li
           v-for="(chapter, index) in menu"
-          class="w-[35vw] px-12 shrink-0"
-          :class="route.name != 'home' ? '' : 'border-l-2 border-black'"
+          class="w-[35vw] shrink-0 h-full"
+          :class="[route.name != 'home' ? '' : 'border-l-2 border-black']"
         >
-          <RouterLink
-            @click="closeMenu()"
-            :to="
-              'the-eye-and-retina' ==
-              menu[index].title.replaceAll(' ', '-').toLowerCase()
-                ? '/chapter/the-eye-and-retina/'
-                : '/chapter/chaptertocome/' +
-                  menu[index].title.replaceAll(' ', '-').toLowerCase()
-            "
+          <ul
+            :class="index == 'Part9' ? 'pointer-events-auto': 'pointer-events-none'"
+            class="pl-6 px-12 w-full h-full overflow-hidden text-black duration-700"
           >
-            <h2 class="pb-5">{{ menu[index].title }}</h2>
-          </RouterLink>
-          <ul class="pl-6" v-for="(part, i) in chapter.parts">
-            <li class="border-b pt-4 border-black hover:bg-gray-100">
-              <RouterLink
-                @click="closeMenu()"
-                :to="{
-                  name: 'chapter',
-                  hash: '#the-eye-and-retina-' + (i + 1),
-                }"
-                >{{ i + " " + part }}</RouterLink
-              >
-            </li>
+            <h2 class="pb-5">
+              {{ menu[index].title }}
+            </h2>
+            <RouterLink
+              :to="{
+                name: 'chapter',
+                hash: '#the-eye-and-retina-' + (i + 1),
+              }"
+            >
+              <li class="border-b pt-4 border-black hover:bg-gray-100">
+                Intro
+              </li>
+            </RouterLink>
+            <RouterLink
+              v-for="(part, i) in chapter.parts"
+              @click="closeMenu()"
+              :to="{
+                name: 'chapter',
+                hash: '#the-eye-and-retina-' + (i + 1),
+              }"
+            >
+              <li class="border-b pt-4 border-black hover:bg-gray-100">
+                {{ i + 1 + " " + part }}
+              </li>
+            </RouterLink>
           </ul>
         </li>
       </ul>
